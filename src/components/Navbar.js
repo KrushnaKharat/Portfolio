5a9b66a88e2084bd6c85293ed9a9febb78566997
Krushna Kharat <krushnakharat.official@gmail.com>
2025-02-14 01:05:24 +0530

    added portfolio

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/Shubh-Danawade/Portfolio"
+                href="https://github.com/KrushnaKharat/Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
