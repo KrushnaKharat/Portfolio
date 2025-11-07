ce4eb9ad94e2f6a771f68968f1f2223318839965
Krishaaa <120458485+KrushnaKharat@users.noreply.github.com>
2025-11-07 17:17:52 +0000

    Add final year project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,15 +1,33 @@
-import { Container } from "react-bootstrap";
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
+import smartresi from "../../Assets/Projects/Smart resi.png"
 
 function Projects() {
   return (
     <Container fluid className="project-section">
       <Particle />
-      <Container style={{ height: "99vh" }}>
-        <h1 className="project-heading h-100">
-          <strong className="purple">Curently Working on Project. </strong>
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
         </h1>
-        <p style={{ color: "white" }}></p>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={smartresi}
+              isBlog={false}
+              title="Smart Residential Services"
+              description="This is a full-stack web application for managing apartment service requests between residents and workers (e.g.
+electricians, plumbers)."
+              // ghLink="https://github.com/soumyajit4419/Chatify"
+              demoLink="https://residential-services.vercel.app/"
+            />
+          </Col>
+        </Row>
       </Container>
     </Container>
   );
